782597ef43c2f366e4d1f0fd42fcea75c8bb1c6b
lainsce <paulogalardi1@gmail.com>
2026-08-13 23:33:53 -0300

    *

--- src/Card/Header/HeaderCardContent.swift
+++ src/Card/Header/HeaderCardContent.swift
@@ -23,7 +23,7 @@ struct HeaderCardContent: View {
                     .foregroundStyle(.primary)
             }
         }
-        .font(.title2.bold())
+        .font(.largeTitle.bold())
         .lineLimit(1)
         .frame(maxWidth: .infinity, alignment: .leading)
         .onChange(of: isEditing) { _, editing in
